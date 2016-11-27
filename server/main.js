b030a22d56c2e92914cf1896192dc11e2c1e3b49
dipu-bd <dipu.sudipta@gmail.com>
2016-11-28 04:57:12 +0600

    initialized full example project

--- server/main.js
+++ server/main.js
@@ -1,5 +1,4 @@
-import { Meteor } from 'meteor/meteor';
+// Server entry point, imports all server code
 
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+import '/imports/startup/server';
+import '/imports/startup/both';
