73421b5824b2cd36e0cb4c28763a2e72ce985898
dipu-bd <dipu.sudipta@gmail.com>
2016-12-02 05:35:31 +0600

    improved begin button at session start page

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,4 +1,7 @@
 // Controller class for App_home
+
+import { Session } from 'meteor/session';
+import { Match } from 'meteor/check';
  
 import "./home.html";
 import './home.scss';
@@ -12,3 +15,19 @@ Template.App_home.onRendered(function () {
         parallaxMultiplier: 4,
     }); 
 }); 
+
+Template.App_home.helpers({
+    beginText() {
+        const user = Session.get("session") || {};
+        if (Match.test(user._id, String)) {
+            return "Resume Your Test";
+        }
+        else {
+            return "Begin Your Test";
+        } 
+    },
+    beginLink() {
+        const user = Session.get("session") || {};
+        return "/session" + (user._id ? "/" + user._id : "");         
+    },
+});
