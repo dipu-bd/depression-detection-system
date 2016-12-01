952df05202716373670deda9f9d9b561997928a6
dipu-bd <dipu.sudipta@gmail.com>
2016-12-02 04:41:34 +0600

    fixed homepage

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -3,5 +3,11 @@
 import "./home.html";
 
 Template.App_home.onRendered(function () {
-    this.$('.particle').particleground();
+    this.$('.particle').particleground({        
+        lineColor: '#669955',
+        dotColor: '#996655',
+        density: 4000, 
+        proximity: 80,
+        parallaxMultiplier: 4,
+    }); 
 }); 
