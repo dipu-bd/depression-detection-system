7c9cdc5012525da02198f8055debd315f3d17edc
= <=>
2017-05-09 05:20:00 +0600

    stats.js: using Meteor.call

--- imports/ui/pages/statistics/stats.js
+++ imports/ui/pages/statistics/stats.js
@@ -6,41 +6,29 @@ import { moment } from 'meteor/momentjs:moment';
 import { _ } from 'meteor/erasaur:meteor-lodash';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import { Choices } from "/imports/api/choices";
-import { Sessions } from '/imports/api/sessions';
-import { SessionCookie } from '/imports/lib/cookies';
-import { Questions } from "/imports/api/questions";
-import { Messages } from '/imports/lib/messages';
-
 import "../../components/loader/loader";
 import "./stats.html";
 import "./stats.scss";
 
 var loading = new ReactiveVar(false);
 
-function calculateEntities() {
+function calculateEntities(self) {
     loading.set(true);
-    let count = 0;
-    Sessions.find().forEach((session)=> {
-        const message = new Messages(session._id);
-        if(message.completed) {
-            count++;
-        }
+    self.autorun(function () {
+        Meteor.call('statistics', functoin(error, result) {
+            loading.set(false);
+            if (error) {
+                console.log(err);
+                Materialize.toast(err, 4000);
+            } else {
+                console.log(result);
+            }
+        });
     });
-    console.log(count, 'complted');
-    loading.set(false);
 }
 
 Template.App_stats.onCreated(function () {
-    // load all questions list
-    this.autorun(() => {
-        this.subscribe('questions.all');
-        this.subscribe('choices.all');
-        this.subscribe('sessions.all');
-        if (this.subscriptionsReady()) {
-            setTimeout(calculateEntities, 100);
-        }
-    });
+    calculateEntities(this);
 });
 
 Template.App_stats.helpers({ 
